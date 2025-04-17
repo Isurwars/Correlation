dddcda043a9149a0bed8bca9508f3106a9707ec6
isurwars <isurwars@gmail.com>
2025-04-17 20:57:59 +0000

    CMAKE migration completed

---
+++ build/src/CMakeFiles/correlation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/correlation/src/Atom.cpp" "src/CMakeFiles/correlation.dir/Atom.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/Atom.cpp.o.d"
+  "/home/user/correlation/src/Cell.cpp" "src/CMakeFiles/correlation.dir/Cell.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/Cell.cpp.o.d"
+  "/home/user/correlation/src/Constants.cpp" "src/CMakeFiles/correlation.dir/Constants.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/Constants.cpp.o.d"
+  "/home/user/correlation/src/ReadFiles.cpp" "src/CMakeFiles/correlation.dir/ReadFiles.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/ReadFiles.cpp.o.d"
+  "/home/user/correlation/src/Smoothing.cpp" "src/CMakeFiles/correlation.dir/Smoothing.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/Smoothing.cpp.o.d"
+  "/home/user/correlation/src/StructureFactor.cpp" "src/CMakeFiles/correlation.dir/StructureFactor.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/StructureFactor.cpp.o.d"
+  "/home/user/correlation/src/WriteFiles.cpp" "src/CMakeFiles/correlation.dir/WriteFiles.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/WriteFiles.cpp.o.d"
+  "/home/user/correlation/src/main.cpp" "src/CMakeFiles/correlation.dir/main.cpp.o" "gcc" "src/CMakeFiles/correlation.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
