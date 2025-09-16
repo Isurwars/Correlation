95900ef2f898d0271e08993b687405e837e3d117
isurwars <isurwars@gmail.com>
2025-09-15 22:28:52 -0600

    Added browse button and Open Dialog functionality

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 // Full license: https://github.com/Isurwars/Correlation/blob/main/LICENSE
 
 #include "../include/AppBackend.hpp"
+#include "../include/PortableFileDialogs.hpp"
 #include "app_window.h"
+
+std::unique_ptr<pfd::open_file> current_file_dialog;
 
 int main() {
   // Create the main window from the Slint UI definition
@@ -32,6 +35,42 @@ int main() {
         static_cast<KernelType>(ui->get_smoothing_kernel());
 
     backend.run_analysis(options);
+  });
+
+  ui->on_browse_file([&]() {
+    // Define the file filters
+    std::vector<std::string> filters = {"Supported Structure Files",
+                                        "*.car *.cell *.cif *.dat",
+                                        "Materials Studio CAR",
+                                        "*.car",
+                                        "CASTEP CELL",
+                                        "*.cell",
+                                        "CIF files",
+                                        "*.cif",
+                                        "ONETEP DAT",
+                                        "*.dat",
+                                        "All Files",
+                                        "*"};
+
+    // Create and show the file dialog, but don't call result() immediately
+    current_file_dialog = std::make_unique<pfd::open_file>(
+        "Select a structure file", "", filters, pfd::opt::multiselect);
+
+    ui->set_timer_running(true);
+  });
+
+  ui->on_check_file_dialog_status([&]() {
+    // Check if the dialog is ready without blocking
+    if (current_file_dialog && current_file_dialog->ready(0)) {
+      // Dialog is ready, get the result
+      auto selection = current_file_dialog->result();
+      if (!selection.empty()) {
+        ui->set_in_file_text(slint::SharedString(selection[0]));
+      }
+      // The dialog is finished, stop the timer and reset the unique_ptr
+      ui->set_timer_running(false);
+      current_file_dialog.reset();
+    }
   });
 
   // Run the Slint event loop
