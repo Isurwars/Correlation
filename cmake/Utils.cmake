61c352315f3280f1b577f7814929dac0f6afe406
isurwars <isurwars@gmail.com>
2026-03-17 21:22:34 -0600

    New try to fix windows workflow

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -6,8 +6,9 @@ function(bundle_windows_dlls target_name)
         add_custom_command(
             TARGET ${target_name} POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            $<TARGET_RUNTIME_DLLS:${target_name}>
-            $<TARGET_FILE_DIR:${target_name}>
+            "$<TARGET_FILE:${target_name}>"
+            "$<TARGET_RUNTIME_DLLS:${target_name}>"
+            "$<TARGET_FILE_DIR:${target_name}>"
             COMMAND_EXPAND_LISTS
             COMMENT "Copying runtime DLLs for ${target_name}"
         )
