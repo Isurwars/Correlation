bdd4cccd984349152bfe1a4379d729ab4edfaf50
isurwars <isurwars@gmail.com>
2026-03-22 19:00:52 -0600

    fix to dll not copying

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -6,6 +6,7 @@ function(bundle_windows_dlls target_name)
         add_custom_command(
             TARGET ${target_name} POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            "$<TARGET_FILE:${target_name}>"
             $<TARGET_RUNTIME_DLLS:${target_name}>
             $<TARGET_FILE_DIR:${target_name}>
             COMMAND_EXPAND_LISTS
