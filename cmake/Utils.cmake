0b3e698ab52e83a7e1b116252581ed4944ffbb68
isurwars <isurwars@gmail.com>
2026-04-02 11:16:34 -0600

    Fix Release action

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -8,9 +8,21 @@ function(bundle_windows_dlls target_name)
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
             "$<TARGET_FILE:${target_name}>"
             $<TARGET_RUNTIME_DLLS:${target_name}>
-            $<TARGET_FILE_DIR:${target_name}>
+            "$<TARGET_FILE_DIR:${target_name}>"
             COMMAND_EXPAND_LISTS
             COMMENT "Copying runtime DLLs for ${target_name}"
         )
+    endif()
+endfunction()
+
+# Ensure that runtime DLLs are correctly included in the CPack installation
+function(install_windows_dlls target_name dest_dir)
+    if(WIN32)
+        # Using a custom install script or directly installing the known TARGET_RUNTIME_DLLS
+        # The generator expression $<TARGET_RUNTIME_DLLS:tgt> is only valid in CMake 3.21+
+        install(FILES $<TARGET_RUNTIME_DLLS:${target_name}>
+                DESTINATION ${dest_dir}
+                COMPONENT Runtime
+                OPTIONAL)
     endif()
 endfunction()
