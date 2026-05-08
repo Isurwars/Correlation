25e72ff58912a9ee7ffa34e159021fc60f04d133
isurwars <isurwars@gmail.com>
2026-05-07 23:20:51 -0600

    chore: update googletest dependency to version 1.17.0

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -133,7 +133,7 @@ else()
   FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.16.1
+    GIT_TAG v1.17.0
   )
   FetchContent_MakeAvailable(googletest)
 
